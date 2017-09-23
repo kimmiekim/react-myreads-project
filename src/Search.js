85ed5804aac56d3e7157e3022fb3a3dbeaf2e81f
Erin Kim <kim.erin.kim@gmail.com>
2017-09-23 14:46:27 -0400

    search filters the list and displays the list

--- src/Search.js
+++ src/Search.js
@@ -1,16 +1,37 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class Search extends Component {
   state={
     query: ''
   }
-  updateQuery = (query) => {
+updateQuery = (query) => {
     this.setState({ query: query.trim()})
-    console.log("this", this)
+    console.log("this", query)
   }
   render() {
+    const query = this.state.query
+    console.log("testing query", typeof query)
+    const books = this.props.books
+
+    let showingBooks
+
+    if(query){
+      console.log("query length", query.length)
+      const match = new RegExp(escapeRegExp(query), 'i')
+      console.log("match",match)
+      showingBooks = books.filter((book) => match.test(book.title))
+    } else {
+      showingBooks = books
+    }
+    showingBooks.sort(sortBy('title'))
+
+
+
     return (
+
       <div className="search-books">
         <div className="search-books-bar">
           <Link to="/" className="close-search">Close</Link>
@@ -23,14 +44,42 @@ class Search extends Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input value={this.state.query} onChange={(event) => this.updateQuery(event.target.value)} type="text" placeholder="Search by title or author"/>
+            <input value={query}
+                    onChange={(event)=> this.updateQuery(event.target.value)}
+                    type="text"
+                    placeholder="Search by title or author"
+                  />
             {JSON.stringify(this.state)}
           </div>
         </div>
-        <div className="search-books-results">
-          <ol className="books-grid"></ol>
-        </div>
+
+        {showingBooks.map((book) => (
+          <div className="search-books-results">
+              <div key={book.id} className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+                  <div className="book-shelf-changer">
+                    <select>
+                      <option value="none" disabled>Move to...</option>
+                      <option value="currentlyReading">Currently Reading</option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors}</div>
+              </div>
+
+          </div>
+          )
+        )}
+        {console.log(showingBooks)}
       </div>
+
+
+
     )
   }
 }
